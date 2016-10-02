30ec53889173a586a83e5d9b40406f0a522e213a
Bart Jacobs <developer@bartjacobs.com>
2016-10-02 10:16:09 +0200

    Populate WeekViewController with weather data

---
+++ Cloudy/View Controllers/Weather View Controllers/Table View Cells/WeatherDayTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherDayTableViewCell.swift
+//  Cloudy
+//
+//  Created by Bart Jacobs on 02/10/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDayTableViewCell: UITableViewCell {
+
+    // MARK: - Type Properties
+
+    static let reuseIdentifier = "WeatherDayCell"
+
+    // MARK: - Properties
+
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var windSpeedLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var iconImageView: UIImageView!
+
+    // MARK: - Initialization
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- Cloudy/View Controllers/Weather View Controllers/WeekViewController.swift
+++ Cloudy/View Controllers/Weather View Controllers/WeekViewController.swift
@@ -12,12 +12,30 @@ class WeekViewController: WeatherViewController {
 
     // MARK: - Properties
 
+    @IBOutlet var tableView: UITableView!
+
+    // MARK: -
+
     var week: [WeatherDayData]? {
         didSet {
             updateView()
         }
     }
-    
+
+    // MARK: -
+
+    fileprivate lazy var dayFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter
+    }()
+
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d"
+        return dateFormatter
+    }()
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -29,13 +47,14 @@ class WeekViewController: WeatherViewController {
     // MARK: - View Methods
 
     private func setupView() {
-
+        setupTableView()
     }
 
     private func updateView() {
         activityIndicatorView.stopAnimating()
 
-        if let _ = week {
+        if let week = week {
+            updateWeatherDataContainer(withWeatherData: week)
 
         } else {
             messageLabel.isHidden = false
@@ -43,5 +62,53 @@ class WeekViewController: WeatherViewController {
             
         }
     }
+
+    // MARK: -
+
+    private func setupTableView() {
+        tableView.separatorInset = UIEdgeInsets.zero
+    }
+
+    // MARK: -
+
+    private func updateWeatherDataContainer(withWeatherData weatherData: [WeatherDayData]) {
+        weatherDataContainer.isHidden = false
+
+        tableView.reloadData()
+    }
     
+}
+
+extension WeekViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return week == nil ? 0 : 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let week = week else { return 0 }
+        return week.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherDayTableViewCell.reuseIdentifier, for: indexPath) as? WeatherDayTableViewCell else { fatalError("Unexpected Table View Cell") }
+
+        if let week = week {
+            // Fetch Weather Data
+            let weatherData = week[indexPath.row]
+
+            // Configure Cell
+            cell.dayLabel.text = dayFormatter.string(from: weatherData.time)
+            cell.dateLabel.text = dateFormatter.string(from: weatherData.time)
+
+            let min = String(format: "%.0f°", weatherData.temperatureMin)
+            let max = String(format: "%.0f°", weatherData.temperatureMax)
+            cell.temperatureLabel.text = "\(min) - \(max)"
+
+            cell.windSpeedLabel.text = String(format: "%.f MPH", weatherData.windSpeed)
+        }
+
+        return cell
+    }
+
 }
