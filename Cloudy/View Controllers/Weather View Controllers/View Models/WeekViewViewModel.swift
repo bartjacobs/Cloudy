59ba71f50446dee3a63665d50a4ca50f56a90b17
Bart Jacobs <developer@bartjacobs.com>
2016-10-05 17:25:45 +0200

    Integrate WeatherDayViewViewModel into WeekViewViewModel

--- Cloudy/View Controllers/Weather View Controllers/View Models/WeekViewViewModel.swift
+++ Cloudy/View Controllers/Weather View Controllers/View Models/WeekViewViewModel.swift
@@ -14,11 +14,6 @@ struct WeekViewViewModel {
 
     let weatherData: [WeatherDayData]
 
-    // MARK: -
-
-    private let dayFormatter = DateFormatter()
-    private let dateFormatter = DateFormatter()
-
     // MARK: - Public Interface
 
     var numberOfSections: Int {
@@ -31,65 +26,8 @@ struct WeekViewViewModel {
 
     // MARK: -
 
-    func day(for index: Int) -> String {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-
-        // Configure Date Formatter
-        dayFormatter.dateFormat = "EEEE"
-
-        return dayFormatter.string(from: weatherDayData.time)
-    }
-
-    func date(for index: Int) -> String {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-
-        // Configure Date Formatter
-        dateFormatter.dateFormat = "MMMM d"
-
-        return dateFormatter.string(from: weatherDayData.time)
-    }
-
-    func temperature(for index: Int) -> String {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-
-        let min = format(temperature: weatherDayData.temperatureMin)
-        let max = format(temperature: weatherDayData.temperatureMax)
-
-        return "\(min) - \(max)"
-    }
-
-    func windSpeed(for index: Int) -> String {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-        let windSpeed = weatherDayData.windSpeed
-
-        switch UserDefaults.unitsNotation() {
-        case .imperial:
-            return String(format: "%.f MPH", windSpeed)
-        case .metric:
-            return String(format: "%.f KPH", windSpeed.toKPH())
-        }
-    }
-
-    func image(for index: Int) -> UIImage? {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-
-        return UIImage.imageForIcon(withName: weatherDayData.icon)
-    }
-
-    // MARK: - Private Interface
-
-    private func format(temperature: Double) -> String {
-        switch UserDefaults.temperatureNotation() {
-        case .fahrenheit:
-            return String(format: "%.0f °F", temperature)
-        case .celsius:
-            return String(format: "%.0f °C", temperature.toCelcius())
-        }
+    func viewModel(for index: Int) -> WeatherDayRepresentable {
+        return WeatherDayViewViewModel(weatherDayData: weatherData[index])
     }
 
 }
